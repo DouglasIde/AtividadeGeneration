954846b70aaa12844f3b06b9a64e003ac4cce792
DouglasIde <douglasymide@gmail.com>
2024-06-04 23:23:38 -0300

    Solucionando o terceiro exercício

--- src/atividadeTres/MainAtividadeTres.java
+++ src/atividadeTres/MainAtividadeTres.java
@@ -1,5 +1,32 @@
 package atividadeTres;
 
-public class MainAtividadeTres {
+import java.util.Scanner;
+import java.util.Locale;
 
+public class MainAtividadeTres {
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner leitura = new Scanner(System.in);
+		
+		float salarioBruto, adicionalNoturno, horasExtras, descontos, salarioLiquido;
+		
+		System.out.println("Salário Bruto: ");
+		salarioBruto = leitura.nextFloat();
+		
+		System.out.println("Adicional Noturno: ");
+		adicionalNoturno = leitura.nextFloat();
+		
+		System.out.println("Horas Extras: ");
+		horasExtras = leitura.nextFloat();
+		
+		System.out.println("Descontos: ");
+		descontos = leitura.nextFloat();
+		
+		salarioLiquido = salarioBruto + adicionalNoturno + (horasExtras * 5) - descontos;
+		
+		System.out.printf("Salário Liquido: %.2f %n", salarioLiquido);
+		
+		
+	}
 }
