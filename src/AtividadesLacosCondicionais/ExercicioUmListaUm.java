d83a3c6562aeb7870015ad9fdda464968484d28d
DouglasIde <douglasymide@gmail.com>
2024-06-05 13:40:40 -0300

    Resolvendo o primeiro exercício de Listas Condicionais da Lista01

---
+++ src/AtividadesLacosCondicionais/ExercicioUmListaUm.java
@@ -0,0 +1,31 @@
+package AtividadesLacosCondicionais;
+
+import java.util.Scanner;
+
+public class ExercicioUmListaUm {
+	public static void main(String[] args) {
+		
+		Scanner leitura = new Scanner(System.in);
+		
+		int A, B, C, calculoAB;
+		
+		System.out.println("Digite o número de A: ");
+		A = leitura.nextInt();
+		
+		System.out.println("Digite o número de B: ");
+		B = leitura.nextInt();
+		
+		System.out.println("Digite o número de C: ");
+		C = leitura.nextInt();
+		
+		calculoAB = A + B;
+		
+		if(calculoAB > C) {
+			System.out.println("A soma de A + B é MAIOR do que C");
+		} else if(calculoAB < C) {
+			System.out.println("A soma de A + B é MENOR do que C");
+		} else if(calculoAB == C) {
+			System.out.println("A soma de A + B é IGUAL a C");
+		}
+	}
+}
