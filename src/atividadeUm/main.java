80a69dcdf079e05c7d7e0551be6a208bdb60cf40
DouglasIde <douglasymide@gmail.com>
2024-06-04 22:16:43 -0300

    first commit

---
+++ src/atividadeUm/main.java
@@ -0,0 +1,30 @@
+package atividadeUm;
+
+import java.util.Scanner;
+import java.util.Locale;
+
+public class main {
+	public static void main(String[] args) {
+		
+		/* 
+		 * Para o código funcionar corretamente, digite com "." 
+		 * (ponto) ao invés de "," (vírgula)
+		 * */
+		
+		Locale.setDefault(Locale.US); 
+		Scanner leitura = new Scanner(System.in);
+		
+		float salario, abono, novoSalario;
+		
+		System.out.println("Digite o Salário: ");
+		salario = leitura.nextFloat();
+		
+		System.out.println("Digite o Abono: ");
+		abono = leitura.nextFloat();
+		
+		novoSalario = salario + abono;
+		
+		System.out.printf("Novo Salário: R$ %.2f %n", novoSalario);
+		
+	}
+}
