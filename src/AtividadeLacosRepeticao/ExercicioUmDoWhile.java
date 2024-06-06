e0cb617c0b9903ccd2cce9bb16b8ff22d44cd89d
DouglasIde <douglasymide@gmail.com>
2024-06-06 20:01:50 -0300

    Solucionando o primeiro exercício da lista 03 (Exercício 5)

---
+++ src/AtividadeLacosRepeticao/ExercicioUmDoWhile.java
@@ -0,0 +1,29 @@
+package AtividadeLacosRepeticao;
+
+import java.util.Scanner;
+
+public class ExercicioUmDoWhile {
+	public static void main(String[] args) {
+
+		/*
+		 * Leia números Inteiros via teclado até que o número 0 seja digitado
+		 * Mostre na tela a soma de todos os números digitados que sejam POSITIVOS
+		 * 
+		 * */
+
+		int numero, soma = 0;
+
+		Scanner leitura = new Scanner(System.in);
+
+		do {
+			System.out.println("Digite um número para ser somado: ");
+			numero = leitura.nextInt();
+
+			if(numero > 0) {
+				soma += numero;
+			}
+			
+		} while(numero != 0);
+		System.out.printf("A soma dos números positivos é: %d", soma);
+	}
+}
