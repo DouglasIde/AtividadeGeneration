98c19c5cf06961c753681b60b6456654615d4b9f
DouglasIde <douglasymide@gmail.com>
2024-06-08 12:58:59 -0300

    Solucionando o segundo exercício da Lista 02 (ex6) do laço SWITCH

--- src/AtividadesLacosCondicionais/ExercicioDoisListaDois.java
+++ src/AtividadesLacosCondicionais/ExercicioDoisListaDois.java
@@ -5,14 +5,25 @@ import java.util.Scanner;
 public class ExercicioDoisListaDois {
 	public static void main(String[] args) {
 		
+		/*
+		 * Leia o Nome do Colaborador (String)
+		 * Leia o Código do Cargo do Colaborador (int 1 - 6)
+		 * Leia o Salário (float)
+		 * Mostre na tela o Nome do Colaborador, Cargo e o novo salário
+		 * 
+		 * */
+		
 		Scanner leitura = new Scanner(System.in);
 		
-		String nomeColaborador;
-		float numero;
+		String nomeColaborador, cargo;
+		float salario, novoSalario, reajuste;
 		int opcao;
 		
 		System.out.println("Digite o nome do Colaborador: ");
 		nomeColaborador = leitura.nextLine();
+		
+		System.out.println("Digite o seu Salário: ");
+		salario = leitura.nextFloat();
 		
 		System.out.println("**************************");
         System.out.println("Selecione um cargo:");
@@ -25,6 +36,62 @@ public class ExercicioDoisListaDois {
         System.out.println("**************************");
         opcao = leitura.nextInt();
         
+        switch(opcao) {
+        case 1:
+        	cargo = "Gerente";
+        	reajuste = 0.10f;
+        	novoSalario = salario + (salario * reajuste);
+            System.out.printf("Nome do Colaborador: %s%n", nomeColaborador);
+            System.out.printf("Cargo: %s%n", cargo);
+            System.out.printf("Salário: %.2f%n", novoSalario);
+        	break;
+        case 2:
+        	cargo = "Vendedor";
+        	reajuste = 0.07f;
+        	novoSalario = salario + (salario * reajuste);
+            System.out.printf("Nome do Colaborador: %s%n", nomeColaborador);
+            System.out.printf("Cargo: %s%n", cargo);
+            System.out.printf("Salário: %.2f%n", novoSalario);
+            break;
+        case 3:
+        	cargo = "Supervisor";
+        	reajuste = 0.09f;
+        	novoSalario = salario + (salario * reajuste);
+            System.out.printf("Nome do Colaborador: %s%n", nomeColaborador);
+            System.out.printf("Cargo: %s%n", cargo);
+            System.out.printf("Salário: %.2f%n", novoSalario);
+            break;
+        case 4:
+        	cargo = "Motorista";
+        	reajuste = 0.06f;
+        	novoSalario = salario + (salario * reajuste);
+            System.out.printf("Nome do Colaborador: %s%n", nomeColaborador);
+            System.out.printf("Cargo: %s%n", cargo);
+            System.out.printf("Salário: %.2f%n", novoSalario);
+            break;
+        case 5:
+        	cargo = "Estoquista";
+        	reajuste = 0.05f;
+        	novoSalario = salario + (salario * reajuste);
+            System.out.printf("Nome do Colaborador: %s%n", nomeColaborador);
+            System.out.printf("Cargo: %s%n", cargo);
+            System.out.printf("Salário: %.2f%n", novoSalario);
+            break;
+        case 6:
+        	cargo = "Técnico de TI";
+        	reajuste = 0.08f;
+        	novoSalario = salario + (salario * reajuste);
+            System.out.printf("Nome do Colaborador: %s%n", nomeColaborador);
+            System.out.printf("Cargo: %s%n", cargo);
+            System.out.printf("Salário: %.2f%n", novoSalario);
+            break;
+            default:
+            	System.out.println("Opção Inválida");
+        }
+        
+
+        
+       
         
        
 	}
