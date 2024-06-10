a4aee78c8eaa4335e3a442e0ca46eac359fe81ce
DouglasIde <douglasymide@gmail.com>
2024-06-10 20:08:58 -0300

    Criando um FOR de interação com o usuário para digitar 10x os valores inteiros

--- src/AtividadesCollections/AtividadeUmListaDoisEx3.java
+++ src/AtividadesCollections/AtividadeUmListaDoisEx3.java
@@ -1,5 +1,30 @@
 package AtividadesCollections;
 
-public class AtividadeUmListaDoisEx3 {
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
 
+public class AtividadeUmListaDoisEx3 {
+	public static void main(String[] args) {
+		
+		/*
+		 * Criar uma Collection SET de objetos de Classe Wrapper Intenger
+		 * O programa deve solicitar ao usuário que ele digite via teclado 10 valores inteiros não repetidos
+		 * Adicione cada valor individualmente na Collection Set
+		 * Mostre na tela todos os elementos da Collection Set (Utilizando a Classe Iterator)
+		 * 
+		 * */
+		
+		Scanner leitura = new Scanner(System.in);
+		
+		Set<Integer> setNumeros = new HashSet<Integer>();
+		
+		int numerosDigitados;
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println("Digite um número: ");
+			numerosDigitados = leitura.nextInt();
+		}
+		
+	}
 }
