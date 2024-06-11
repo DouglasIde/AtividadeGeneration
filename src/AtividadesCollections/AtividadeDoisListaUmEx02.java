02fb5452339e6c69bf6a212f3081cf38ea4304dd
DouglasIde <douglasymide@gmail.com>
2024-06-10 22:54:29 -0300

    Definindo as tarefas para serem realizadas no exercício 03

--- src/AtividadesCollections/AtividadeDoisListaUmEx02.java
+++ src/AtividadesCollections/AtividadeDoisListaUmEx02.java
@@ -1,5 +1,15 @@
 package AtividadesCollections;
 
 public class AtividadeDoisListaUmEx02 {
-
+	public static void main(String[] args) {
+		
+		/*
+		 * Criar uma Collection ArrayList de Objetos da Classe Wrapper Integer
+		 * O ArrayList inicializa com 10 valores inteiros
+		 * O programa deve solicitar ao usuário que ele digite 1 número inteiro via teclado
+		 * Caso ele seja encontrada no ArrayList exiba na tela a posição deste número na Collection
+		 * Caso o número NÃO seja encontrado, exiba na tela: "O número NN não foi encontrado!"
+		 * 
+		 * */
+	}
 }
