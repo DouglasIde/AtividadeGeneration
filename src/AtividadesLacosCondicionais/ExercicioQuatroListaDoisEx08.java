99683b820a39699fd690870ad8a28432bc43b439
DouglasIde <douglasymide@gmail.com>
2024-06-11 07:42:36 -0300

    Atribuindo as tarefas no exercício 08 de Laços Condicionais

--- src/AtividadesLacosCondicionais/ExercicioQuatroListaDoisEx08.java
+++ src/AtividadesLacosCondicionais/ExercicioQuatroListaDoisEx08.java
@@ -5,5 +5,17 @@ import java.util.Scanner;
 public class ExercicioQuatroListaDoisEx08 {
 	public static void main(String[] args) {
 		
+		/* 
+		 * Desenvolver uma Conta Bancária
+		 * O programa deve ler o tipo de operação a ser realizada (int 1 - 3)
+		 * Valor a ser depositado ou sacado (somente nas opções 2 e 3)
+		 * Um saque só pode ser realizado caso haja saldo sulficiente
+		 * Ao final de cada operação exiba o novo Saldo na tela
+		 * A variável FLOAT será iniciada com R$ 1000.0
+		 * Caso a operação seja diferente do intervalo 1 a 3, mostre a mensagem ("Operação Inválida")
+		 * 
+		 * */
+		
+		
 	}
 }
