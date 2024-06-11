d247ff781619912860905ab751d582fa8671d908
DouglasIde <douglasymide@gmail.com>
2024-06-11 08:55:54 -0300

    Atribuindo as tarefas que o sistema irá realizar

--- src/AtividadesLacosCondicionais/ExercícioQuatroListaUmEx04.java
+++ src/AtividadesLacosCondicionais/ExercícioQuatroListaUmEx04.java
@@ -1,5 +1,14 @@
 package AtividadesLacosCondicionais;
 
-public class ExercícioQuatroListaUmEx04 {
+import java.util.Scanner;
 
+public class ExercícioQuatroListaUmEx04 {
+	public static void main(String[] args) {
+		
+		/*
+		 * Escreva um algoritmo que leia 3 palavras (STRING)
+		 * Cada palavra definem as características de um tipo de animal
+		 * 
+		 * */
+	}
 }
