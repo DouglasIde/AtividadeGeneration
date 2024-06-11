f32173fc46bf3c42fa5a20793881b9ed21bd3867
DouglasIde <douglasymide@gmail.com>
2024-06-11 14:08:41 -0300

    Criando o menu do exercício 01

--- src/AtividadesFila/ExercicioUmListaUmEx01.java
+++ src/AtividadesFila/ExercicioUmListaUmEx01.java
@@ -1,5 +1,19 @@
 package AtividadesFila;
 
 public class ExercicioUmListaUmEx01 {
-
+	public static void main(String[] args) {
+		
+		int opcao;
+		
+		System.out.println("""
+				*********************************
+				1 - Adicionar Cliente na Fila
+				2 - Listar todos os Clientes
+				3 - Retirar Cliente da Fila
+				0 - Sair
+				*********************************
+				Entre com a opção desejado:
+				""");
+	
+	}
 }
