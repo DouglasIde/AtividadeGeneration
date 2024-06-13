d47345ebc2f2d01a2c9fc5b7e9db73b148d14978
DouglasIde <douglasymide@gmail.com>
2024-06-13 07:39:48 -0300

    Adicionando Dentro do laço invertebrado o inseto

--- src/AtividadesLacosCondicionais/ExercícioQuatroListaUmEx04.java
+++ src/AtividadesLacosCondicionais/ExercícioQuatroListaUmEx04.java
@@ -16,6 +16,8 @@ public class ExercícioQuatroListaUmEx04 {
 		String vertebrado = "vertebrado",invertebrado = "invertebrado";
 		String tipoAnimal, aveOuMamifero, aveCarnivoroOuOnivoro;
 		String herbivoroOuOnivoro;
+		String insetoOuAnelideo;
+		String hematofagoOuHerbivoro, hematofagoOuOnivoro;
 
 		System.out.println("Digite um tipo de Animal (Ex: Vertebrado ou Invertebrado): ");
 		tipoAnimal = leitura.nextLine();
@@ -24,7 +26,7 @@ public class ExercícioQuatroListaUmEx04 {
 			System.out.println("Ele(a) é um Mamífero ou uma Ave? : ");
 			aveOuMamifero = leitura.nextLine();
 			if(aveOuMamifero.equalsIgnoreCase("ave")) {
-				System.out.println("Ele(a) é Carnívoro ou Onívoro");
+				System.out.println("Ele(a) é Carnívoro ou Onívoro? ");
 				aveCarnivoroOuOnivoro = leitura.nextLine();
 				if(aveCarnivoroOuOnivoro.equalsIgnoreCase("carnívoro")) {
 					System.out.println("O seu animal é uma ÁGUIA");
@@ -32,12 +34,24 @@ public class ExercícioQuatroListaUmEx04 {
 					System.out.println("O seu animal é uma POMBA");
 				}
 			} else if(aveOuMamifero.equalsIgnoreCase("mamifero")) {
-				System.out.println("Ele(a) é Herbivoro ou Onivoro");
+				System.out.println("Ele(a) é Herbivoro ou Onivoro? ");
 				herbivoroOuOnivoro = leitura.nextLine();
 				if(herbivoroOuOnivoro.equalsIgnoreCase("herbivoro")) {
 					System.out.println("O seu animal é uma VACA");
 				} else if(herbivoroOuOnivoro.equalsIgnoreCase("onivoro")) {
 					System.out.println("O seu animal é um HOMEM");
+				}
+			}
+		} else if(tipoAnimal.equalsIgnoreCase(invertebrado)) {
+			System.out.println("Ele(a) é um Inseto ou Anelideo? : ");
+			insetoOuAnelideo = leitura.nextLine();
+			if(insetoOuAnelideo.equalsIgnoreCase("inseto")) {
+				System.out.println("Ele é Hematofago ou Herbivoro? ");
+				hematofagoOuHerbivoro = leitura.nextLine();
+				if(hematofagoOuHerbivoro.equals("hematofago")) {
+					System.out.println("O seu animal é uma PULGA");
+				} else if(hematofagoOuHerbivoro.equalsIgnoreCase("herbivoro")) {
+					System.out.println("O seu animal é uma LAGARTA");
 				}
 			}
 		}
