b4b9158c7e2ee7b27d0403764edc76d62b671cd3
DouglasIde <douglasymide@gmail.com>
2024-06-14 07:56:02 -0300

    Adicionando o menu para identidade de genero exercício 4 Laços de Repetições

--- src/AtividadeLacosRepeticao/ExercicioDoisWhile.java
+++ src/AtividadeLacosRepeticao/ExercicioDoisWhile.java
@@ -8,6 +8,27 @@ public class ExercicioDoisWhile {
 		// TODO Auto-generated method stub
 
 		Scanner leitura = new Scanner(System.in);
+
+		int idade;
+		int identidadeGenero;
+
+		System.out.println("Digite sua idade: ");
+		idade = leitura.nextInt();
+		
+		System.out.println("""
+				
+			Qual a sua identidade de Gênero: 
+			
+			1 - Mulher Cis
+			2 - Homem Cis
+			3 - Não Binário
+			4 - Mulher Trans
+			5 - Homem Trans
+			6 - Outros
+			
+			Escolha a sua resposta:
+		""");
+		identidadeGenero = leitura.nextInt();
 	}
 
 }
