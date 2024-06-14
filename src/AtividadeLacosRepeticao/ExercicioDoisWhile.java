5b0c77d6f3c6ff07c83b59b40ee50bdfdc019e5b
DouglasIde <douglasymide@gmail.com>
2024-06-13 22:59:13 -0300

    Adicionando o Scanner no Laço de Repetição WHILE

---
+++ src/AtividadeLacosRepeticao/ExercicioDoisWhile.java
@@ -0,0 +1,13 @@
+package AtividadeLacosRepeticao;
+
+import java.util.Scanner;
+
+public class ExercicioDoisWhile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner leitura = new Scanner(System.in);
+	}
+
+}
