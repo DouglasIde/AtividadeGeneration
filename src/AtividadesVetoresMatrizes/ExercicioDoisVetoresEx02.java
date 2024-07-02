66690d5521fd9b3f6692c6ca156ce02a2ec22d24
DouglasIde <douglasymide@gmail.com>
2024-07-01 22:47:52 -0300

    Adicionando a pasta IMG para armazenar as imagens para o README

---
+++ src/AtividadesVetoresMatrizes/ExercicioDoisVetoresEx02.java
@@ -0,0 +1,19 @@
+package AtividadesVetoresMatrizes;
+
+public class ExercicioDoisVetoresEx02 {
+	public static void main(String[] args) {
+		
+		/*
+		 * Ler 10 números inteiros e armazene-os em um vetor
+		 * Mostre na tela:
+		 * 		Todos os elementos dos índices ímpares do vetor
+		 * 		Todos os elementos do vetor que são números pares
+		 * 		A SOMA de todos os elementos do vetor
+		 * 		A MÉDIA de todos os elementos do vetor, armazenada em uma variavel do tipo real
+		 * 
+		 * */
+		
+		
+	}
+
+}
